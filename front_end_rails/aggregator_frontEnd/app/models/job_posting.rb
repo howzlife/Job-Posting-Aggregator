918f1965389f1d9cfdfabef1e26f1a2e4f016b8a
howzlife <nico_dubus@hotmail.com>
2014-12-26 15:55:18 -0500

    added tests for job posting validation

--- front_end_rails/aggregator_frontEnd/app/models/job_posting.rb
+++ front_end_rails/aggregator_frontEnd/app/models/job_posting.rb
@@ -1,4 +1,6 @@
 class JobPosting < ActiveRecord::Base
 	validates_presence_of :job_ID, :keyword, :company, :city, :province, :country
-
+	
+	validates :job_ID, format: { with: /\A[0-9][0-9][0-9][0-9][0-9][0-9]+\z/,
+    message: "Job ID Must be 6 Integer Digits" }
 end
--- front_end_rails/aggregator_frontEnd/test/models/job_posting_test.rb
+++ front_end_rails/aggregator_frontEnd/test/models/job_posting_test.rb
@@ -11,4 +11,18 @@ class JobPostingTest < ActiveSupport::TestCase
 	  assert_not job_posting.valid?
 	  assert_equal [:job_ID, :keyword, :company, :city, :province, :country], job_posting.errors.keys
 	end
+
+	test "new job posting should have all fields with input" do
+		job_posting = JobPosting.new(job_ID: '101010', keyword: 'CakePHP', company: 'Public Works', city: 'Toronto', province: 'Ontario', country: 'Canada')
+		assert job_posting.valid?
+	end
+
+	test "new job posting should have 6 integers in job id" do 
+		job_posting = JobPosting.new(job_ID: '10101', keyword: 'CakePHP', company: 'Public Works', city: 'Toronto', province: 'Ontario', country: 'Canada')
+		assert_not job_posting.valid?
+		job_posting2 = JobPosting.new(job_ID: '101015', keyword: 'CakePHP', company: 'Public Works', city: 'Toronto', province: 'Ontario', country: 'Canada')
+		assert job_posting2.valid?
+	end
+
+	
 end
