eaa12d6797bc0a2dc15dc44cf8422eed4a1c4364
howzlife <nico_dubus@hotmail.com>
2015-01-11 12:43:12 -0500

    added charts from chartkick, removed queries scaffold, query now done in job_postings

--- front_end_rails/aggregator_frontEnd/config/application.rb
+++ front_end_rails/aggregator_frontEnd/config/application.rb
@@ -1,10 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'jquery-rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+
+
+Bundler.require(*Rails.groups(:assets => %w(development test)))
 
 module AggregatorFrontend
   class Application < Rails::Application
